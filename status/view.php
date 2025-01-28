22c4c5cb4d87541d60ce6b6796976b27967f5e84
Lynxprazo <issamwanga02@gmail.com>
2025-01-28 12:12:24 +0300

    form edit submission

--- status/view.php
+++ status/view.php
@@ -1,12 +1,27 @@
 <?php 
  include "../databaseconnection.php";
  try{
-   $sql = $conn->query("SELECT * ")
+    $sql = $conn->query("SELECT * FROM team_membar");
+    $result = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+    if($result){
+      echo json_encode($result);
+   }else{
+      echo json_encode(["message"=>"no data is found"]);
+      exit;
+   }
 
  }catch(PDOException $e){
     die("Something went wrong".$e->getMessage());
 
  }
+// Remove  and Update the  the detail of member of Team 
 
+try{
+   
+
+}catch(PDOException $e){
+  die("Something went Wrong".$e->getMessage());
+}
 
 ?>
